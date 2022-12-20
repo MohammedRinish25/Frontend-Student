33db9cf155e6989a0e42e9b5263e7fa872eacf2f
Mohammed Rinish A <rinish.a@acheron-tech.com>
2022-12-21 00:10:25 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,20 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddUpdateComponent } from './components/add-update/add-update.component';
+import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { StudentListComponent } from './components/student-list/student-list.component';
+import { StudentDetailsComponent } from './components/student-details/student-details.component';
+import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddUpdateComponent,
+    DashboardComponent,
+    StudentListComponent,
+    StudentDetailsComponent,
+    NavBarComponent
   ],
   imports: [
     BrowserModule,
